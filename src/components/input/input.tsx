98020b705539508a3d4762f330b04fd7b4588fac
Simon <snsimonescobar@gmail.com>
2022-02-06 01:55:03 -0500

    fix for deploy again

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState, useContext } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import '../../assets/styles/input.css';
 // import AppContext from '../AppContext';
 import ModalCreateActivity from '../Modal/ModalCreateActivity'; 
